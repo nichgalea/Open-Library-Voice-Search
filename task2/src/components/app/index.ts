f3a396853f71c84b51b7c416b7f80dce2919a2eb
Nicholas Galea <nich.galea@gmail.com>
2019-05-19 20:41:02 +0200

    handle search request error

--- task2/src/components/app/index.ts
+++ task2/src/components/app/index.ts
@@ -19,7 +19,6 @@ export class AppComponent extends HTMLElement {
 
   connectedCallback() {
     this.information = document.createElement("div");
-    this.information.innerText = "We didn't find anything matching that query! :/";
     this.information.className = cx(styles.information, styles.hidden);
 
     const search = new SearchComponent(this.handleResult, this.handleError);
@@ -37,9 +36,17 @@ export class AppComponent extends HTMLElement {
     if (result.docs.length > 0) {
       this.information.className = cx(styles.information, styles.hidden);
     } else {
+      this.information.innerText = "We didn't find anything matching that query! :/";
       this.information.className = styles.information;
     }
   }
 
-  private handleError() {}
+  private handleError() {
+    if (this.carousel) {
+      this.carousel.items = [];
+    }
+
+    this.information.className = styles.information;
+    this.information.innerText = "There was an error while getting your results!";
+  }
 }
